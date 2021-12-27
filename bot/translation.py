0aa8cdab121bb1c5ab3d97e9bc91fae2ec033a0d
DFF <96736687+DFF01@users.noreply.github.com>
2021-12-27 22:44:49 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,6 +7,8 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
+How are You Baby 🤣
+
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
 For More Details Click Help Button Below..
